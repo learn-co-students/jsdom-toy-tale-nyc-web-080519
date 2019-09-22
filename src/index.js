a157106f7bd6f136b9c3f95d4ea1a1dc89c9b9a4
github username <github email address>
2019-09-22 11:38:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,80 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+const collection = document.querySelector("#toy-collection")
 
-// YOUR CODE HERE
+// -------------------------------------------
 
+function getToys() {
+  return fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+}
+
+function appendToy(toyData) {
+  collection.insertAdjacentHTML("beforeend", `
+      <div class="card">
+        <h2>${toyData.name}</h2>
+        <img src=${toyData.image} class="toy-avatar" />
+        <p>${toyData.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+      </div>
+    `) 
+}
+
+function newToy(toyData){
+  // post the data to the url
+  data = fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    // (dont forget to specify the keys of the data when you're passing it)
+    body: JSON.stringify({
+      name: toyData.name.value,
+      image: toyData.image.value,
+      likes: "0"
+    })
+  })
+  // convert from string to JSON
+  .then(resp => resp.json())
+  // use our other method that takes in the object we just created and displays it
+  .then( jsonOBJ => appendToy(jsonOBJ))
+}
+
+// function addLike(){
+//   data = fetch("http://localhost:3000/toys/:id",{
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     },
+//     body: {
+//       "likes": 0
+//     }
+// })
+
+        // -------------------------------------------
+        
 addBtn.addEventListener('click', () => {
-  // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
+    // IMPORTANT: when we are submitting, the target is the data that is being sent, not the button itself (like on line 30)
+    toyForm.addEventListener('submit', e => {
+      e.preventDefault();
+      newToy(e.target)
+    })
   } else {
     toyForm.style.display = 'none'
   }
 })
 
+// -------------------------------------------
 
-// OR HERE!
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    appendToy(toy)
+  })
+})
