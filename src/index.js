93a69fdb05391c55eee9961f868854ded165127c
kgriswo1 <kgriswo1@binghamton.edu>
2019-09-23 11:24:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toy_collection = document.querySelector("#toy-collection")
 let addToy = false
 
-// YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -14,5 +14,79 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+fetch("http://localhost:3000/toys")
+.then(function (response) {
+  return response.json()
+})
+.then(renderToy)
 
-// OR HERE!
+function renderToy(data) {
+  data.forEach(renderOneToy)
+}
+
+function renderOneToy(toy) {
+  const str = `
+    <div class="card">
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar">
+      <p><span class="like-display">${toy.likes}</span> Likes</p>
+      <button data-action="like" data-id="${toy.id}" class="like-btn">Like</button>
+    </div>`
+  toy_collection.insertAdjacentHTML("beforeend", str)
+}
+
+toyForm.addEventListener("submit", function(event) {
+  event.preventDefault()
+  
+  const name = event.target.name.value
+  const image = event.target.image.value
+  
+  const body = {
+    name: name, 
+    image: image,
+    likes: 0
+  }
+  
+  fetch("http://localhost:3000/toys", {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json",
+      Accepts: "application/json"
+    },
+    body: JSON.stringify(body) 
+  })
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    renderOneToy(data)
+  })
+  
+  document.querySelectorAll(".input-text").forEach(function(textbox){
+    textbox.value = ''
+  })
+})
+
+toy_collection.addEventListener("click", function(event) {
+  if(event.target.dataset.action === "like") {
+    const span = event.target.closest("div.card").querySelector("span.like-display")
+    const newLikes = parseInt(span.innerText) + 1
+
+    fetch(`http://localhost:3000/toys/${event.target.dataset.id}`, {
+      method: "PATCH", 
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json"
+      },
+      body: JSON.stringify({likes: newLikes})
+    })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      span.innerText = newLikes
+    })
+  }
+})
+
+
