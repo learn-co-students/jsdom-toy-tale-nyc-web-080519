fb02920e47f849cc3198bca848bafd42b8c3ccda
github username <github email address>
2019-09-25 00:03:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,80 +1,98 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
-const collection = document.querySelector("#toy-collection")
+document.addEventListener("DOMContentLoaded", () => {
 
-// -------------------------------------------
+  const collection = document.querySelector("#toy-collection")
+  const newButton = document.querySelector("#new-toy-btn")
+  const form = document.querySelector(".add-toy-form")
+  const container = document.querySelector(".container")
+  let addToy = false
+  let data = []
+  
+  start()
 
-function getToys() {
-  return fetch("http://localhost:3000/toys")
-    .then(resp => resp.json())
-}
-
-function appendToy(toyData) {
-  collection.insertAdjacentHTML("beforeend", `
-      <div class="card">
-        <h2>${toyData.name}</h2>
-        <img src=${toyData.image} class="toy-avatar" />
-        <p>${toyData.likes} Likes </p>
-        <button class="like-btn">Like <3</button>
-      </div>
-    `) 
-}
-
-function newToy(toyData){
-  // post the data to the url
-  data = fetch("http://localhost:3000/toys", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    // (dont forget to specify the keys of the data when you're passing it)
-    body: JSON.stringify({
-      name: toyData.name.value,
-      image: toyData.image.value,
-      likes: "0"
+  function start() {
+    getToys().then(toys => {
+      data = toys
+      data.forEach(toy => {
+        renderOneToy(toy)
+      })
     })
-  })
-  // convert from string to JSON
-  .then(resp => resp.json())
-  // use our other method that takes in the object we just created and displays it
-  .then( jsonOBJ => appendToy(jsonOBJ))
-}
-
-// function addLike(){
-//   data = fetch("http://localhost:3000/toys/:id",{
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json"
-//     },
-//     body: {
-//       "likes": 0
-//     }
-// })
-
-        // -------------------------------------------
-        
-addBtn.addEventListener('click', () => {
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // IMPORTANT: when we are submitting, the target is the data that is being sent, not the button itself (like on line 30)
-    toyForm.addEventListener('submit', e => {
-      e.preventDefault();
-      newToy(e.target)
-    })
-  } else {
-    toyForm.style.display = 'none'
   }
-})
 
-// -------------------------------------------
+  function getToys() {
+    return fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+  }
+
+  function renderOneToy(toyOBJ) {
+    collection.insertAdjacentHTML("beforeend", `
+      <div class="card" id="${toyOBJ.id}">
+        <h2>${toyOBJ.name}</h2>
+        <img src="${toyOBJ.image}" class="toy-avatar"/>
+        <p>${toyOBJ.likes}</p>
+        <button class="like-btn" id="${toyOBJ.id}">  Like <3  </button>
+      </div>
+    `)
+  }
 
 
-getToys().then(toys => {
-  toys.forEach(toy => {
-    appendToy(toy)
+  function newToy(toyOBJ) {
+    debugger
+    return fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        name: toyOBJ.name,
+        image: toyOBJ.image,
+        likes: "0"
+      })
+    })
+  }
+
+  function renderLike(obj) {
+    obj.likes = obj.likes + 1
+    renderOneToy(obj)
+  }
+
+  newButton.addEventListener("click", e => {
+    addToy = !addToy
+    if (addToy) {
+      container.style.display = "block"
+    } else {
+      container.style.display = "none"
+    }
   })
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    name = container.querySelector("#formName").value;
+    url = container.querySelector("#formIMG").value;
+    newToy({ name: name, image: url })
+  })
+
+
+  // I decided to create an event listener that also does the fetch, because it can
+  // use e.target.previousElementSibling to grab the likes <p> and update it with the newLike
+
+  // normally, i would break this out into a method of its own
+
+  document.addEventListener("click", e => {
+    if (e.target.className === "like-btn") {
+      const newLike = parseInt(e.target.previousElementSibling.innerText) + 1
+      debugger
+      fetch(`http://localhost:3000/toys/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": newLike
+        })
+      }).then(e.target.previousElementSibling.innerText = newLike)
+    }
+  })
+
 })
